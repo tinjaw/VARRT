9f988fa9205144e7e9120233ee0e02970161a851
Chaim Krause <chaim@chaim.com>
2020-09-25 07:34:45 -0500

    Moved Vassal page into the Home page.

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -4,7 +4,6 @@ import { Switch, Route } from 'react-router-dom';
 import routes from './constants/routes.json';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
-import VassalPage from './containers/VassalPage';
 
 // Lazily load routes and code split with webpack
 const LazyCounterPage = React.lazy(() =>
@@ -22,7 +21,6 @@ export default function Routes() {
     <App>
       <Switch>
         <Route path={routes.COUNTER} component={CounterPage} />
-        <Route path={routes.VASSAL} component={VassalPage} />
         <Route path={routes.HOME} component={HomePage} />
       </Switch>
     </App>
--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,18 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes.json';
+import { remote } from 'electron';
+import { promises } from 'fs';
 import styles from './Home.css';
 
 export default function Home(): JSX.Element {
+  async function getFile() {
+    const filename = await remote.dialog.showOpenDialog({
+      properties: ['openFile'],
+    });
+    return promises.readFile(filename.filePaths[0]).then((value) => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      return JSON.parse(value);
+    });
+  }
+
+  function getOnClick() {
+    async function getClick() {
+      const foo = await getFile();
+      // eslint-disable-next-line no-console
+      console.log(`We finish with ${JSON.stringify(foo)}`);
+    }
+    return getClick;
+  }
+
   return (
     <div className={styles.container} data-tid="container">
-      <h2>Home</h2>
-      <p>
-        <Link to={routes.COUNTER}>to Counter</Link>
-      </p>
-      <p>
-        <Link to={routes.VASSAL}>to Vassal</Link>
-      </p>
+      <button
+        className="f3 link dim br2 ph3 pv2 mb2 dib white bg-dark-red"
+        onClick={getOnClick()}
+        data-tclass="btn"
+        type="button"
+      >
+        <i className="far fa-play-circle" />
+        &nbsp;Easy Button
+      </button>
     </div>
   );
 }
