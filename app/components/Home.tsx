4cacfdc551349a9bdb15073e5274deb753aff717
Chaim Krause <chaim@chaim.com>
2020-11-11 15:00:23 -0600

    Added Easy Button
    Added Easy Button and moved splash img

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -2,5 +2,11 @@ import React from 'react';
 import styles from './Home.css';
 
 export default function Home(): JSX.Element {
-  return <div className={styles.container} data-tid="container" />;
+  return (
+    <div className={styles.container} data-tid="container">
+      <button type="button" className="btn btn-primary">
+        Easy Button
+      </button>
+    </div>
+  );
 }
